b1b01cc6a0a3b691cf9fd19d099c72d8633ad302
yersonargote <yersonargote@unicauca.edu.co>
2023-09-22 15:11:36 -0500

    First version of remove newline of a given text

---
+++ main.py
@@ -0,0 +1,30 @@
+import pyperclip
+
+def remove_newlines(text):
+    """Función que elimina los saltos de línea de un texto."""
+    return text.replace('\n', ' ')
+
+def main():
+    print("Por favor, introduce el texto. Cuando hayas terminado, escribe 'EOF' en una nueva línea y presiona Enter:")
+
+    lines = []
+    while True:
+        line = input()
+        if line == "EOF":
+            break
+        lines.append(line)
+
+    text = '\n'.join(lines)
+    modified_text = remove_newlines(text)
+    
+    # Mostrar el texto modificado en consola
+    print("\nTexto modificado:")
+    print(modified_text)
+    
+    # Copiar el texto modificado al portapapeles
+    pyperclip.copy(modified_text)
+    print("\nEl texto modificado ha sido copiado al portapapeles.")
+
+if __name__ == "__main__":
+    main()
+
