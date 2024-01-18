129fdd422ea93ada83f6f32508424118cd494601
yersonargote <yersonargotev@unicauca.edu.co>
2024-01-17 22:43:51 -0500

    Add help messages to txt and pdf commands

--- main.py
+++ main.py
@@ -7,24 +7,32 @@ from pdf.service import run_pdf_action
 from text.enums import TXTActions
 from text.service import run_text_action
 
-app = typer.Typer()
+app = typer.Typer(help="Herramienta para manipular archivos de texto y PDFs.")
 
 
-@app.command("txt")
+@app.command(
+    "txt",
+    help="Manipular texto plano. Acciones: wrdc, rmln.",
+)
 def txt(
     action: Annotated[
         TXTActions,
-        typer.Argument(help="Acción a realizar con el texto. Ex: wrdc, rm_newln"),
+        typer.Argument(
+            help="Acciones: [wrdc] para contar palabras, [rmln] para remover saltos de línea.",
+        ),
     ],
 ):
     run_text_action(action)
 
 
-@app.command("pdf")
+@app.command(
+    "pdf",
+    help="Manipular archivos PDF. Acciones: split.",
+)
 def pdf(
     action: Annotated[
         PDFActions,
-        typer.Argument(help="Acción a realizar con el PDF., Ex: split"),
+        typer.Argument(help="Acciones: [split] para extraer páginas dado un rango."),
     ],
 ):
     run_pdf_action(action)
